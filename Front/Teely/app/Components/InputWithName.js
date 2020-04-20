52674a7dcb9c539bd352be96bbc1303bc71e0430
Louis <ung.louis@hotmail.fr>
2020-04-20 12:22:20 +0200

    completed backend readme + removed font style for now

--- Front/Teely/app/Components/InputWithName.js
+++ Front/Teely/app/Components/InputWithName.js
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
 
   },
   text: {
-    fontFamily: 'Cochin',
+    // fontFamily: 'Cochin',
     fontSize: 16,
     textAlign : 'center',
     color: 'black',  
--- Front/Teely/app/Components/SubmitButton.js
+++ Front/Teely/app/Components/SubmitButton.js
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
   buttonText : {
     fontSize: 20,
     color : 'black',
-    fontFamily:'Cochin',
+    //fontFamily:'Cochin',
     fontWeight : 'bold'
   },
 
