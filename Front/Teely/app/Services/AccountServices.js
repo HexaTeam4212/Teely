4bfaf7d09391dbc4c6a74619009f226b5a951a4e
Louis <ung.louis@hotmail.fr>
2020-04-23 07:09:16 +0200

    view + services OK

--- Front/Teely/app/Services/AccountServices.js
+++ Front/Teely/app/Services/AccountServices.js
@@ -1,6 +1,6 @@
 //app/Services/SignUpService.js
 
-const backendURL = "http://811122ea.ngrok.io"
+const backendURL = "http://18045fd1.ngrok.io"
 const endpoint = "/account/"
 
 class AccountServices {
@@ -51,6 +51,37 @@ class AccountServices {
             catch (error) {
                 console.error(error)
             }
+        }
+    }
+
+    async login(username, password, callback){
+        try {
+            const requestBody = JSON.stringify({
+                username: username,
+                password: password,
+            })
+            const fullEndpoint = endpoint + "login"
+            const response = await fetch(backendURL + fullEndpoint, 
+            {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: requestBody
+            })
+            if (response.status != 200) {
+                if (response.status == 400) {
+                    alert("Nom d'utilisateur ou mot de passe non renseignés")
+                    callback(false);
+                }    
+            }
+            else {
+                callback(true);
+            }
+        }
+        catch (error) {
+            console.error(error)
         }
     }
 
--- Front/Teely/app/Views/Login.js
+++ Front/Teely/app/Views/Login.js
@@ -1,44 +1,74 @@
-// app/Views/SignUp.js
+// app/Views/Login.js
 import React from 'react'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
-import { StyleSheet, Text, View, Image, ActivityIndicator} from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
+
+import SubmitButton from '../Components/SubmitButton'
+import InputWithName from '../Components/InputWithName'
 import accountServices from '../Services/AccountServices';
-export default class SignUp extends React.Component {
+
+export default class Login extends React.Component {
   constructor(props) {
     super(props)
+    this.state = { hasError: false };
+    this.username = ""
+    this.password = ""
   }
 
+  callbackFunctionUsername = (childData) => {
+    this.username = childData
+  }
 
+  callbackFunctionPassword = (childData) => {
+    this.password = childData
+  }
 
+  login = () => {
+    accountServices.login(this.username, this.password, (connexionOK) => {
+      if (connexionOK == true) {
+        this.props.navigation.navigate("Profile")
+      }
+    })
+  }
 
   render() {
-    return(
-    <View style={styles.main_container}>
-      <KeyboardAwareScrollView 
-        contentContainerstyle={styles.content_container} 
-        resetScrollToCoords={{ x: 0, y: 0 }}
-        scrollEnabled={true}
-        enableAutomaticScroll={(Platform.OS === 'ios')}
-        enableOnAndroid={true}>
-        
-      </KeyboardAwareScrollView>
-    </View>
+    return (
+      <View style={styles.main_container}>
+        <KeyboardAwareScrollView
+          contentContainerstyle={styles.content_container}
+          resetScrollToCoords={{ x: 0, y: 0 }}
+          scrollEnabled={true}
+          enableAutomaticScroll={(Platform.OS === 'ios')}
+          enableOnAndroid={true}>
+
+          <InputWithName name="Nom d'utilisateur" type='username' placeholder='Pseudonyme'
+            secureTextEntry={false} parentCallback={this.callbackFunctionUsername} />
+          <InputWithName name='Mot de passe' type='password' placeholder='********'
+            secureTextEntry={true} parentCallback={this.callbackFunctionPassword} />
+          <SubmitButton name='Se connecter' onPress={this.login} />
+          <Text style={styles.text}> Pas encore de compte ? </Text>
+          <Button title='Inscription' onPress={() => {
+            this.props.navigation.navigate("SignUp")
+          }} />
+        </KeyboardAwareScrollView>
+      </View>
     )
   }
+
 }
 
 const styles = StyleSheet.create({
-  main_container : {
+  main_container: {
     backgroundColor: '#78e1db',
-    flex: 1 
+    flex: 1
   },
-  content_container : {
-    flex:1,
-    marginTop : 30,
-    marginBottom : 10,
-    flexDirection : 'column',
+  content_container: {
+    flex: 1,
+    marginTop: 30,
+    marginBottom: 10,
+    flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'center'    
+    alignItems: 'center'
   },
-  
+
 });
