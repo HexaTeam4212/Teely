0f406bbf4e45d22f38c73984cd3dedfd8639b49a
Louis <ung.louis@hotmail.fr>
2020-04-24 17:58:26 +0200

    changed alert msg for 401 status code

--- Front/Teely/app/modules/Error.js
+++ Front/Teely/app/modules/Error.js
@@ -1,7 +1,7 @@
 export function httpError(statusCode) {
     switch(statusCode) {
         case 401:
-            alert("Votre session a expiré. Veuillez vous reconnecter.")
+            alert("Erreur d'authentification.")
             break
         case 500:
             alert("Une erreur s'est produite au niveau du serveur. Veuillez réessayer plus tard ou contacter le support informatique.")
