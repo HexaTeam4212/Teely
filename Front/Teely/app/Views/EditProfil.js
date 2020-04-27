ee999e9a8e3475f65dd93b5621cabae26d29e69d
Louis <ung.louis@hotmail.fr>
2020-04-27 02:13:03 +0200

    improved editprofile view's style

--- Front/Teely/app/Views/EditProfil.js
+++ Front/Teely/app/Views/EditProfil.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import NameWithInput from '../Components/NameWithInput'
 import DateTimePicker from '../Components/DateTimePicker'
-import { StyleSheet, View, Image, TouchableOpacity, Text, Platform, ActivityIndicator } from 'react-native';
+import { StyleSheet, View, Image, TouchableOpacity, Text, Platform, ActivityIndicator, ScrollView } from 'react-native';
 import CustomButton from '../Components/CustomButton'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import accountServices from '../Services/AccountServices';
@@ -88,13 +88,13 @@ export default class EditProfil extends React.Component {
     else {
       this.setState({ isLoading: true })
       accountServices.saveProfil(this.state.username, this.state.password, this.state.confirmedPassword,
-        this.state.email, this.state.lastName, this.state.name, this.birthDate, this.state.biography, 
+        this.state.email, this.state.lastName, this.state.name, this.birthDate, this.state.biography,
         this.redirect)
     }
-    
+
   }
 
-  getDataProfil = ()=> {
+  getDataProfil = () => {
     if (this.firstLoadPage) {
       console.log("firstloadPage")
       this.initDatasProfil = accountServices.dataProfil()
@@ -123,39 +123,42 @@ export default class EditProfil extends React.Component {
   render() {
     return (
       <View style={styles.main_container}>
-        <View style={styles.image_container}>
-          {this.imageProfil()}
-        </View>
+
         <View style={styles.content_container}>
-        <KeyboardAwareScrollView
-        contentContainerstyle={styles.content_container}
-        resetScrollToCoords={{ x: 0, y: 0 }}
-        scrollEnabled={true}
-        enableAutomaticScroll={(Platform.OS === 'ios')}
-        enableOnAndroid={true}>
-        <NameWithInput name='Nom : ' type='none' placeholder={this.initDatasProfil[0]} height={40}
-          value={this.state.lastName} secureTextEntry={false} parentCallback={this.callbackFunctionLastName} />
-        <NameWithInput name='Prénom : ' type='none' placeholder={this.initDatasProfil[1]} height={40}
-          value={this.state.name} secureTextEntry={false} parentCallback={this.callbackFunctionName} />
-        <NameWithInput name="Nom d'utilisateur : " type='username' placeholder={this.initDatasProfil[2]} height={40}
-          value={this.state.username} secureTextEntry={false} parentCallback={this.callbackFunctionUsername} />
-        <NameWithInput name='Mot de passe : ' type='password' placeholder={this.initDatasProfil[3]} height={40}
-          value={this.state.password} secureTextEntry={true} parentCallback={this.callbackFunctionPassword} />
-        <NameWithInput name={"Confirmez votre \nmot de passe : "} type='password' placeholder={this.initDatasProfil[3]} height={40}
-          value={this.state.confirmedPassword} secureTextEntry={true} parentCallback={this.callbackFunctionName} />
-        <NameWithInput name='Email : ' type='emailAddress' placeholder={this.initDatasProfil[4]} height={40}
-          value={this.state.email} secureTextEntry={false} parentCallback={this.callbackFunctionEmail} />
-        <View style={styles.date_container} >
-          <Text style={[styles.text, { marginRight: 10 }]}>Date de naissance : </Text>
-          <DateTimePicker name={this.initDatasProfil[5]} marginRight={35} parentCallback={this.callbackFunctionBirthDate} />
+          <KeyboardAwareScrollView
+            contentContainerstyle={styles.content_container}
+            resetScrollToCoords={{ x: 0, y: 0 }}
+            scrollEnabled={true}
+            enableAutomaticScroll={(Platform.OS === 'ios')}
+            enableOnAndroid={true}
+          >
+            <View style={styles.image_container}>
+              {this.imageProfil()}
+            </View>
+            <NameWithInput name='Nom : ' type='none' placeholder={this.initDatasProfil[0]} height={40}
+              value={this.state.lastName} secureTextEntry={false} parentCallback={this.callbackFunctionLastName} />
+            <NameWithInput name='Prénom : ' type='none' placeholder={this.initDatasProfil[1]} height={40}
+              value={this.state.name} secureTextEntry={false} parentCallback={this.callbackFunctionName} />
+            <NameWithInput name={"Nom \nd'utilisateur : "} type='username' placeholder={this.initDatasProfil[2]} height={40}
+              value={this.state.username} secureTextEntry={false} parentCallback={this.callbackFunctionUsername} />
+            <NameWithInput name='Mot de passe : ' type='password' placeholder={this.initDatasProfil[3]} height={40}
+              value={this.state.password} secureTextEntry={true} parentCallback={this.callbackFunctionPassword} />
+            <NameWithInput name={"Confirmez votre \nmot de passe : "} type='password' placeholder={this.initDatasProfil[3]} height={40}
+              value={this.state.confirmedPassword} secureTextEntry={true} parentCallback={this.callbackFunctionName} />
+            <NameWithInput name='Email : ' type='emailAddress' placeholder={this.initDatasProfil[4]} height={40}
+              value={this.state.email} secureTextEntry={false} parentCallback={this.callbackFunctionEmail} />
+            <View style={styles.date_container} >
+              <Text style={[styles.text, { marginLeft: 40, marginRight : 15 }]}>Date de {'\n'}naissance :</Text>
+              <DateTimePicker name={this.initDatasProfil[5]} marginRight={35} parentCallback={this.callbackFunctionBirthDate} />
+            </View>
+            <NameWithInput name='Biographie : ' type='none' placeholder={this.initDatasProfil[6]} height={200}
+              value={this.state.biography} secureTextEntry={false} parentCallback={this.callbackFunctionBibliography} />
+            <View style={styles.buttons_container}>
+              <CustomButton name="Enregistrer" onPress={this.saveProfil} />
+            </View>
+          </KeyboardAwareScrollView>
         </View>
-        <NameWithInput name='Biographie : ' type='none' placeholder={this.initDatasProfil[6]} height={200}
-          value={this.state.biography} secureTextEntry={false} parentCallback={this.callbackFunctionBibliography} />
-      </KeyboardAwareScrollView>
-        </View>
-        <View style={styles.buttons_container}>
-          <CustomButton name="Enregistrer" onPress={this.saveProfil} />
-        </View>
+
         {this.displayLoading()}
       </View>
     )
@@ -170,9 +173,9 @@ const styles = StyleSheet.create({
   content_container: {
     flex: 7,
     margin: 5,
-    borderColor: '#ffdb58',
-    borderRadius: 10,
-    borderWidth: 5,
+    // borderColor: '#ffdb58',
+    // borderRadius: 10,
+    // borderWidth: 5,
   },
   image_container: {
     flex: 2,
