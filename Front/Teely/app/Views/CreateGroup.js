15b47daca457f97702b9a53a1e7d144a4d75e6d2
Louis <ung.louis@hotmail.fr>
2020-04-28 19:03:19 +0200

    view creategroup in progress

--- Front/Teely/app/Views/CreateGroup.js
+++ Front/Teely/app/Views/CreateGroup.js
@@ -1,10 +1,12 @@
 // app/Views/CreateGroup.js
 import React from 'react'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
-import { StyleSheet, Text, View, Image, ActivityIndicator, TextInput} from 'react-native'
+import { StyleSheet, Text, View, Image, ActivityIndicator, TextInput } from 'react-native'
 
 import InputWithName from '../Components/InputWithName'
-
+import Images from '../modules/ImageProfile'
+import InviteButton from '../Components/InviteButton'
+import CustomButton from '../Components/CustomButton'
 
 export default class CreateGroup extends React.Component {
   constructor(props) {
@@ -24,35 +26,48 @@ export default class CreateGroup extends React.Component {
   }
 
   render() {
-    return(
-    <View style={styles.main_container}>
-      <KeyboardAwareScrollView 
-        contentContainerstyle={styles.content_container} 
-        resetScrollToCoords={{ x: 0, y: 0 }}
-        scrollEnabled={true}
-        enableAutomaticScroll={(Platform.OS === 'ios')}
-        enableOnAndroid={true}>
-        <InputWithName placeholder='Nom du groupe' value={this.state.groupName} parentCallback={this.callbackFunctionGroupName}/>
-        <InputWithName style={styles.text} placeholder={'Description\n\n\n'} value={this.state.description} parentCallback={this.callbackFunctionDescription}
-        multiline={true}/>
-      </KeyboardAwareScrollView>
-    </View>
+    return (
+      <View style={styles.main_container}>
+        <KeyboardAwareScrollView
+          resetScrollToCoords={{ x: 0, y: 0 }}
+          scrollEnabled={true}
+          enableAutomaticScroll={(Platform.OS === 'ios')}
+          enableOnAndroid={true}>
+          <Image style={styles.profile} source={Images[2]} />
+          <InputWithName placeholder='Nom du groupe' value={this.state.groupName} parentCallback={this.callbackFunctionGroupName} />
+          <InputWithName style={styles.text} placeholder={'Description\n\n\n'} value={this.state.description}
+            parentCallback={this.callbackFunctionDescription} multiline={true} />
+          <InviteButton name='Inviter des participants'></InviteButton>
+           {/* flatlist for adding dynamically invitations */}
+          <View style={styles.create_container}>
+            <CustomButton name='Créer groupe'></CustomButton>
+          </View>
+        </KeyboardAwareScrollView>
+      </View>
+
     )
   }
 }
 
 const styles = StyleSheet.create({
-  main_container : {
+  main_container: {
     backgroundColor: '#78e1db',
     flex: 1
   },
-  content_container : {
-    flex:1,
-    marginTop : 30,
-    marginBottom : 10,
-    flexDirection : 'column',
-    justifyContent: 'center',
-    alignItems: 'center'
+  profile: {
+    resizeMode: 'contain',
+    alignItems: 'center',
+    alignSelf: 'center',
+    width: 90,
+    height: 90,
+    borderColor: '#ffb4e2',
+    borderWidth: 3,
+    borderRadius: 60,
+    margin: 10
+  },
+  create_container: {
+    flex: 1,
+    marginTop: 30
   }
-  
+
 });
