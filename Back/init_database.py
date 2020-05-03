fa86d99673f42b2737173cb86dc99c79dc3bee71
Baptiste Lotigier <baptiste.lotigier@gmail.com>
2020-05-03 12:41:45 +0200

    change task to have 2 datetimefield start and end

--- Back/init_database.py
+++ Back/init_database.py
@@ -35,10 +35,9 @@ class TASK(BaseModel):
     Description = CharField(max_length = 500, null = True)
     Frequency = IntegerField(null = True)
     Group = ForeignKeyField(GROUP, backref='+')
-    Date = DateField(formats = '%Y-%m-%d', null = True)
+    DatetimeStart = DateTimeField(formats = '%Y-%m-%d %H:%M:%S', null = True)
+    DatetimeEnd = DateTimeField(formats = '%Y-%m-%d %H:%M:%S', null = True)
     PriorityLevel = IntegerField(null = True)
-    Duration = IntegerField()
-    StartingTime = IntegerField()
 
 class DEPENDANCE(BaseModel):
     DependanceId = AutoField(primary_key = True)
