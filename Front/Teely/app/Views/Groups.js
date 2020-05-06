26078df660caf4e2a71ee47f1570ea3b735b4a64
Emmylrd <emmylerandy@gmail.com>
2020-05-06 02:19:47 -0400

    Volet mais problème de flex

--- Front/Teely/app/Views/Groups.js
+++ Front/Teely/app/Views/Groups.js
@@ -1,7 +1,7 @@
 // app/Views/Groups.js
 import React from 'react'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
-import { RefreshControl, StyleSheet, Text, View, Image, FlatList, TouchableOpacity, ActivityIndicator } from 'react-native'
+import { RefreshControl, StyleSheet, Text, View, Image, FlatList, TouchableOpacity, ActivityIndicator, Button } from 'react-native'
 
 import groupServices from '../Services/GroupServices';
 import accountServices from '../Services/AccountServices';
@@ -9,6 +9,8 @@ import ImagesGroup from '../modules/ImageGroup'
 import GroupItem from '../Components/GroupItem'
 import ImageWithText from '../Components/ImageWithText'
 import ProfileIcon from '../Components/ProfileIcon'
+import MenuButton from '../Components/MenuButton'
+import MenuDrawer from 'react-native-side-drawer'
 export default class Groups extends React.Component {
   constructor(props) {
     super(props)
@@ -16,7 +18,7 @@ export default class Groups extends React.Component {
     this.groups = []
     this.invitations = []
     this.firstload = true
-    this.state = { isLoading: true, refreshing: false, idImageProfile: 18 }
+    this.state = { isLoading: true, refreshing: false, idImageProfile: 18, open : false}
 
     this.getInvitations()
     this.getDataProfile()
@@ -108,35 +110,73 @@ export default class Groups extends React.Component {
       )
     }
   }
+ 
+  toggleOpen = () => {
+    this.setState({ open: !this.state.open });
+  }
 
+  drawerContent = () => {
+    return (
+      <View style={styles.menu}>
+        <TouchableOpacity onPress={this.toggleOpen}  style={{flex : 1}} >
+          <ProfileIcon idImage={this.state.idImageProfile} /> 
+        </TouchableOpacity> 
+        <View style={{flex : 12}}>
+          <MenuButton name='Profil' width={200}  onPress={() => this.props.navigation.navigate("Profile")}/>
+          <MenuButton name='Calendrier' width={200} onPress={() => this.props.navigation.navigate("PersonalCalendar")}/>
+          <MenuButton name='Groupes' width={200} onPress={() => this.props.navigation.navigate("Groups")}/>
+          <MenuButton name='Invitations' width={200} onPress={() => this.props.navigation.navigate("Invitations", { invitations: this.invitations })}/> 
+          <MenuButton name='Paramètres' width={200} onPress={() => this.props.navigation.navigate("EditProfile")}/> 
+          <MenuButton name='Déconnexion' width={200} onPress={() => {
+                  accountServices.logout()
+                  this.props.navigation.navigate("Login")
+              }} />
+        </View>
+      </View>
+    )
+  }
 
   render() {
     return (
       <View style={styles.main_container}>
+        
         <View style={{ flex: 1 }}>
-          <KeyboardAwareScrollView
-            contentContainerStyle={{ flex: 1 }}
-            resetScrollToCoords={{ x: 0, y: 0 }}
-            scrollEnabled={true}
-            enableAutomaticScroll={(Platform.OS === 'ios')}
-            enableOnAndroid={true}
-            refreshControl={<RefreshControl refreshing={this.state.refreshing} onRefresh={this.onRefresh} />}
+          <MenuDrawer 
+              open={this.state.open} 
+              drawerContent={this.drawerContent()}
+              drawerPercentage={50}
+              animationTime={250}
+              overlay={false}
+              opacity={0.2}
           >
-            <ProfileIcon idImage={this.state.idImageProfile} />
-            <View style={styles.title_container}>
-              <Text style={styles.title_text}>Mes groupes</Text>
-              <TouchableOpacity onPress={() => this.props.navigation.navigate("CreateGroup")}>
-                <Image style={styles.image_plus} source={require('../../assets/Images/plus.png')} />
-              </TouchableOpacity>
-            </View>
-            <View style={styles.content_container}>
-              {this.displayGroups()}
-            </View>
-            <View style={styles.invit_container}>
-              {this.displayInvitations()}
-            </View>
-          </KeyboardAwareScrollView>
+          
+            <KeyboardAwareScrollView
+              contentContainerStyle={{ flex: 1 }}
+              resetScrollToCoords={{ x: 0, y: 0 }}
+              scrollEnabled={true}
+              enableAutomaticScroll={(Platform.OS === 'ios')}
+              enableOnAndroid={true}
+              refreshControl={<RefreshControl refreshing={this.state.refreshing} onRefresh={this.onRefresh} />}
+            >
+              <TouchableOpacity onPress={this.toggleOpen} >
+                <ProfileIcon idImage={this.state.idImageProfile} /> 
+              </TouchableOpacity> 
+              <View style={styles.title_container}>
+                <Text style={styles.title_text}>Mes groupes</Text>
+                <TouchableOpacity onPress={() => this.props.navigation.navigate("CreateGroup")}>
+                  <Image style={styles.image_plus} source={require('../../assets/Images/plus.png')} />
+                </TouchableOpacity>
+              </View>
+              <View style={styles.content_container}>
+                {this.displayGroups()}
+              </View>
+              <View style={styles.invit_container}>
+                {this.displayInvitations()}
+              </View>
+            </KeyboardAwareScrollView>
+          </MenuDrawer>
         </View>
+
         {this.displayLoading()}
       </View>
     )
@@ -149,7 +189,7 @@ const styles = StyleSheet.create({
     flex: 1
   },
   title_container: {
-    flex: 2,
+    flex: 1,
     flexDirection: 'row',
     alignItems: 'flex-end',
     justifyContent: 'flex-start',
@@ -162,7 +202,7 @@ const styles = StyleSheet.create({
   },
   content_container: {
     flex: 8,
-    marginBottom: 10,
+    marginBottom: 5,
     marginTop: 10,
     flexDirection: 'column',
     justifyContent: 'center',
@@ -181,7 +221,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#60dbd3',
   },
   invit_container: {
-    flex: 3,
+    flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'flex-start',
@@ -236,6 +276,12 @@ const styles = StyleSheet.create({
     bottom: 100,
     alignItems: 'center',
     justifyContent: 'center'
+  },  
+  menu: {
+    flex : 1,
+    flexDirection: 'column',
+    backgroundColor: "#ffb4e2",
+    padding: 0
   },
 
 });
